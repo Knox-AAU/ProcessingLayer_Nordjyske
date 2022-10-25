23bec92643a467c1337e91d09c32e4cfa83abc43
Martin Langgaard Jacobsen <martin.l.jacobsen2010@live.dk>
2022-10-25 16:07:23 +0200

    Splitting and in/output worked on
    Working on splitting input list of document id's and their word vectors.
    
    Redesigned input and output to be a list of lists

--- article/find_nearest_document.py
+++ article/find_nearest_document.py
@@ -1,31 +1,114 @@
 import numpy as np
 from scipy.spatial import distance
 
-def find_nearest_docs(x1):
-    for i in x1:
-        nearest_docs = [
-            
+
+# def find_nearest_docs(x):
+#     for i in x:
+#         id_and_vec = x[i-1]
+#         nearest_document_list = [].append(id)
+#         for j in x:
+#             dist = np.linalg.norm(arr-arra)
+#             np_array = np.array([[i][j]])
+#             return distance.pdist(np_array, 'euclidean')
+    
+#     return expected_output
+   
+
+expected_output = [
+            [
+                1,
+                [
+                    2,
+                    15.297058540778355
+                ],
+                [
+                    3,
+                    19.621416870348583
+                ]
+            ],
+            [
+                2,
+                [
+                    1,
+                    15.297058540778355
+                ],
+                [
+                    3,
+                    11.0
+                ]
+            ],
+            [
+                3,
+                [
+                    1,
+                    19.621416870348583
+                ],
+                [
+                    2,
+                    11.0
+                ]
+            ]
         ]
-        for j in x2:
-            np_array = np.array([[i][j]])
-            return distance.pdist(np_array, 'euclidean')
-        
 
-test_arrays = np.random.rand(100,2000) #test arrays, 100 stk, 2000 dimensioner
-#print(np.random.rand(100,2000))
 
-for i in test_arrays:
-    for j in test_arrays:
-        find_nearest_docs(i)
+#print(find_nearest_docs(1))
+
+
+
+
+
+#sort_array = np.array([0,1,5,19,7,69,420, 1.4, 21, 12, 1])
+#print(np.sort(sort_array)) #Returnerer et sorteret array nemt.
+
+#print(np.array_split(sort_array, 5))
+
+
+input_list = [[1,[1,2,3,4,5,6,7,8,9,10]],[2,[10,9,8,7,10,9,8,7,6,5]],[3,[11,11,11,11,11,11,11,11,11,11]]]
+
+# x = 0
+# for i in input_list:
+#     id_doc = input_list[x][0]
+#     id_vec = input_list[x][1]
+#     print(id_doc)
+#     print(id_vec)
+#     x=x+1
     
 
-
-
-sort_array = np.array([0,1,5,19,7,69,420, 1.4, 21, 12, 1])
-print(np.sort(sort_array)) #Returnerer et sorteret array nemt.
-
-print(np.array_split(sort_array, 5))
-
-
-#def find_nearest_docs():
+i = 0
+j=0
+similarity_array = []
+while i < len(input_list):
+    id_main_doc = input_list[i][0]
+    similarity_array.append(id_main_doc)
     
+    for j in input_list[j][1]:
+        arr = j
+        arra = j+1
+        print(j)
+        j=j+1
+        #print(j+1)
+        dist = np.linalg.norm(arr-arra)
+        similarity_array.append(dist)
+        break
+        # print(input_list[i][0])
+        # print(input_list[i][1])
+    #print(similarity_array)
+    i = i + 1
+
+# input_list_ny = [[1],[2],3,4,5,6,7,8,9,10]
+# for i in input_list_ny:
+#     ##k = j+1
+#     id_doc = input_list_ny[i]
+#     id_vec = input_list_ny[i]
+#     #id_vec = input_list[j][k]
+#     print(id_doc + id_vec)
+#     #print(id_vec)
+
+
+#[id, [id, dist],[id, dist],[id, dist],[id, dist]]
+
+#print(id)
+
+#dist = np.linalg.norm(arr-arra)
+#print(dist)
+
--- tests/nearest_document_test.py
+++ tests/nearest_document_test.py
@@ -5,31 +5,47 @@ from article.find_nearest_document import find_nearest_docs
 
 
 class TestGetNearestDocs(unittest.TestCase):
-
-    def test_get_nearest_docs(self):
-        test_data = [
-        {
-            1,
-            [np.random.rand(1,2000)]
-        },
-        {
-            2,
-            [np.random.rand(1,2000)]
-        },
-        {
-            3,
-            [np.random.rand(1,2000)]
-        },
-        {
-            4,
-            [np.random.rand(1,2000)]
-        },
-        {
-            5,
-            [np.random.rand(1,2000)]
-        }
-        ]
-        self.assertEqual(find_nearest_docs(test_data), [1, {}])
         
+    def test_get_nearest_docs(self):
+        input = [[1,[1,2,3,4,5,6,7,8,9,10]],[2,[10,9,8,7,10,9,8,7,6,5]],[3,[11,11,11,11,11,11,11,11,11,11]]]
+        expected = [
+            [
+                1,
+                [
+                    2,
+                    15.297058540778355
+                ],
+                [
+                    3,
+                    19.621416870348583
+                ]
+            ],
+            [
+                2,
+                [
+                    1,
+                    15.297058540778355
+                ],
+                [
+                    3,
+                    11.0
+                ]
+            ],
+            [
+                3,
+                [
+                    1,
+                    19.621416870348583
+                ],
+                [
+                    2,
+                    11.0
+                ]
+            ]
+        ]
+        output = find_nearest_docs(input)
+        self.assertListEqual(output, expected)
+        #np.testing.assert_array_equal(output, expected)
+
     if __name__ == '__main__':
-    unittest.main()
+        unittest.main()
