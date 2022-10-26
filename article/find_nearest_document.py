1eafbf6fd1515269bec0b94ed5c0bc199879be5e
knielsen-afk <knie20@student.aau.dk>
2022-10-26 12:14:55 +0200

    Can now find distance of other documents in correct data structure

--- article/find_nearest_document.py
+++ article/find_nearest_document.py
@@ -1,18 +1,5 @@
 import numpy as np
-from scipy.spatial import distance
-
-
-# def find_nearest_docs(x):
-#     for i in x:
-#         id_and_vec = x[i-1]
-#         nearest_document_list = [].append(id)
-#         for j in x:
-#             dist = np.linalg.norm(arr-arra)
-#             np_array = np.array([[i][j]])
-#             return distance.pdist(np_array, 'euclidean')
-    
-#     return expected_output
-   
+from scipy.spatial import distance   
 
 expected_output = [
             [
@@ -50,65 +37,19 @@ expected_output = [
             ]
         ]
 
-
-#print(find_nearest_docs(1))
-
-
-
-
-
-#sort_array = np.array([0,1,5,19,7,69,420, 1.4, 21, 12, 1])
-#print(np.sort(sort_array)) #Returnerer et sorteret array nemt.
-
-#print(np.array_split(sort_array, 5))
-
-
-input_list = [[1,[1,2,3,4,5,6,7,8,9,10]],[2,[10,9,8,7,10,9,8,7,6,5]],[3,[11,11,11,11,11,11,11,11,11,11]]]
-
-# x = 0
-# for i in input_list:
-#     id_doc = input_list[x][0]
-#     id_vec = input_list[x][1]
-#     print(id_doc)
-#     print(id_vec)
-#     x=x+1
-    
+input_list = [[1,[1,2,3,4,5,6,7,8,9,10]],[2,[10,9,8,7,10,9,8,7,6,5]],[3,[11,11,11,11,11,11,11,11,11,11]],[4,[13,5,1,55,22,12,7,9,33,2]]]
 
 i = 0
-j=0
 similarity_array = []
-while i < len(input_list):
+for j in input_list:
     id_main_doc = input_list[i][0]
-    similarity_array.append(id_main_doc)
-    
-    for j in input_list[j][1]:
-        arr = j
-        arra = j+1
-        print(j)
-        j=j+1
-        #print(j+1)
-        dist = np.linalg.norm(arr-arra)
-        similarity_array.append(dist)
-        break
-        # print(input_list[i][0])
-        # print(input_list[i][1])
-    #print(similarity_array)
+    similarity_array.append([id_main_doc])
+    arr = np.array(j[1])
+    for k in input_list:
+        if (k[1] != j[1]):
+            arra = np.array(k[1])
+            dist = np.linalg.norm(arr-arra)
+            similarity_array[i].append([k[0], dist])
     i = i + 1
-
-# input_list_ny = [[1],[2],3,4,5,6,7,8,9,10]
-# for i in input_list_ny:
-#     ##k = j+1
-#     id_doc = input_list_ny[i]
-#     id_vec = input_list_ny[i]
-#     #id_vec = input_list[j][k]
-#     print(id_doc + id_vec)
-#     #print(id_vec)
-
-
-#[id, [id, dist],[id, dist],[id, dist],[id, dist]]
-
-#print(id)
-
-#dist = np.linalg.norm(arr-arra)
-#print(dist)
-
+    
+print(similarity_array)
