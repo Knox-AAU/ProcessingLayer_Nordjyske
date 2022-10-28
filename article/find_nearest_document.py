f9c603c7b1312a861afa49124762d1c698644133
knielsen-afk <knie20@student.aau.dk>
2022-10-28 11:05:35 +0200

    Removed test list and added test file

--- article/find_nearest_document.py
+++ article/find_nearest_document.py
@@ -3,8 +3,6 @@ import numpy as np
 
 NEAREAST_DOCS_AMOUNT = 5
 
-input_list = [[1,[1,2,3,4,5,6,7,8,9,10]],[2,[10,9,8,7,10,9,8,7,6,5]],[3,[11,11,11,11,11,11,11,11,11,11]],[4,[13,5,1,55,22,12,7,9,33,2]],[5,[31,32,29,12,9,7,7,7,0,2]],[6,[0,11,1,1,14,99,4,29,33,21]],[7,[1,0,0,0,17,15,13,11,9,7]],[8,[12,1,29,40,17,19,88,6,62,8]],[9,[55,0,8,3,3,51,55,0,0,0]],[10,[0,72,74,3,3,51,55,0,0,0]]]
-
 def get_neareast_docs(input):
     i = 0
     similarity_array = []
@@ -21,5 +19,4 @@ def get_neareast_docs(input):
                 id_similarity.sort(key=itemgetter(1))
         similarity_array[i].append(id_similarity[0:NEAREAST_DOCS_AMOUNT])
         i = i + 1
-    #print(similarity_array)
     return similarity_array
