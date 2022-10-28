f65a7517175ec1d01fb1daa7ca415a2105ab4890
knielsen-afk <knie20@student.aau.dk>
2022-10-28 12:03:33 +0200

    changed the naming of several elements

--- article/find_nearest_document.py
+++ article/find_nearest_document.py
@@ -3,20 +3,18 @@ import numpy as np
 
 NEAREAST_DOCS_AMOUNT = 5
 
-def get_neareast_docs(input):
-    i = 0
-    similarity_array = []
-    for j in input:
-        id_main_doc = input[i][0]
-        similarity_array.append([id_main_doc])
-        main_doc = np.array(j[1])
+def get_neareast_docs(id_and_word_count_arr):
+    nearest_docs_array = []
+    for index, main_doc in enumerate(id_and_word_count_arr):
+        id_main_doc = id_and_word_count_arr[index][0]
+        nearest_docs_array.append([id_main_doc])
+        main_doc_word_count = np.array(main_doc[1])
         id_similarity = []
-        for k in input:
-            if (k[1] != j[1]):
-                other_doc = np.array(k[1])
-                dist = np.linalg.norm(main_doc-other_doc)
-                id_similarity.append([k[0], dist])
+        for other_doc in id_and_word_count_arr:
+            if other_doc[1] != main_doc[1]:
+                other_doc_word_count = np.array(other_doc[1])
+                dist = np.linalg.norm(main_doc_word_count-other_doc_word_count)
+                id_similarity.append([other_doc[0], dist])
                 id_similarity.sort(key=itemgetter(1))
-        similarity_array[i].append(id_similarity[0:NEAREAST_DOCS_AMOUNT])
-        i = i + 1
-    return similarity_array
+        nearest_docs_array[index].append(id_similarity[0:NEAREAST_DOCS_AMOUNT])
+    return nearest_docs_array
