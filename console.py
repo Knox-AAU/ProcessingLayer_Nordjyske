e647b1ae96a690f21b4a4129fdd38d6e32d0bba4
Jakob Frederik Lykke <JakobJFL@gmail.com>
2022-11-08 12:10:38 +0100

    update colores

--- console.py
+++ console.py
@@ -1,27 +1,33 @@
 from datetime import datetime
 import sys
 import os
-from colorama import Fore
 
 WARNING_TEXT = ''
 
+class ConColors:
+    BLUE = '\033[94m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    WHITE = '\u001b[37m'
+    MAGENTA = '\u001b[35m'
+
 def print_warning(msg):
     global WARNING_TEXT
-    WARNING_TEXT += f'{Fore.YELLOW}\nWARNING: {Fore.WHITE}' + msg
+    WARNING_TEXT += f'{ConColors.YELLOW}\nWARNING: {ConColors.WHITE}' + msg
 
 def print_error(msg):
-    print(f'{Fore.RED}ERROR: {Fore.WHITE}'+msg)
+    print(f'{ConColors.RED}ERROR: {ConColors.WHITE}'+msg)
 
 def update_status_console(num_of_files, current_file, start_time):
     os.system('cls')
-    print(f'{Fore.BLUE}File ' + str(current_file) + ' of ' + str(num_of_files))
-    print(f'{Fore.WHITE}Time passed: '+ str(datetime.now() - start_time))
+    print(f'{ConColors.BLUE}File ' + str(current_file) + ' of ' + str(num_of_files))
+    print(f'{ConColors.WHITE}Time passed: '+ str(datetime.now() - start_time))
     if WARNING_TEXT != '':
-        print(f'{Fore.YELLOW}Warnings: ' + WARNING_TEXT)
+        print(f'{ConColors.YELLOW}Warnings: ' + WARNING_TEXT)
 
 def console_confirmation():
-    print(f'{Fore.MAGENTA}Are you sure you want to insert all articles in the directory?')
-    print(f'{Fore.WHITE}Make sure the database does not have data in it')
+    print(f'{ConColors.MAGENTA}Are you sure you want to insert all articles in the directory?')
+    print(f'{ConColors.WHITE}Make sure the database does not have data in it')
     input_str = input('Type "yes": ')
     if input_str == 'yes':
         return
