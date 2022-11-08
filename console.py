43bf979d5199bfe8de61e6042f3efb1850226615
Jakob Frederik Lykke <JakobJFL@gmail.com>
2022-11-08 11:54:29 +0100

    Added better Warnings

--- console.py
+++ console.py
@@ -3,8 +3,11 @@ import sys
 import os
 from colorama import Fore
 
+WARNING_TEXT = ''
+
 def print_warning(msg):
-    print(f'{Fore.YELLOW}WARNING: {Fore.WHITE}'+msg)
+    global WARNING_TEXT
+    WARNING_TEXT += f'{Fore.YELLOW}\nWARNING: {Fore.WHITE}' + msg
 
 def print_error(msg):
     print(f'{Fore.RED}ERROR: {Fore.WHITE}'+msg)
@@ -13,6 +16,8 @@ def update_status_console(num_of_files, current_file, start_time):
     os.system('cls')
     print(f'{Fore.BLUE}File ' + str(current_file) + ' of ' + str(num_of_files))
     print(f'{Fore.WHITE}Time passed: '+ str(datetime.now() - start_time))
+    if WARNING_TEXT != '':
+        print(f'{Fore.YELLOW}Warnings: ' + WARNING_TEXT)
 
 def console_confirmation():
     print(f'{Fore.MAGENTA}Are you sure you want to insert all articles in the directory?')
