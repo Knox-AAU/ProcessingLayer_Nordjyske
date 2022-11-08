92608910e4b08b8c5eb97973d9ca0d5c537d613c
Jakob Frederik Lykke <JakobJFL@gmail.com>
2022-11-08 12:20:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from data_api.insert import insert_articles_tokens
 from console import print_error, update_status_console, console_confirmation
 from exceptions import HttpException
 
-TEST_DATA_PATH = 'jsonTestData/'
+DATA_PATH = 'jsonTestData/'
 
 def process_insert_articles(path, current_file, start_time):
-    update_status_console(len(os.listdir(TEST_DATA_PATH)), current_file, start_time)
+    update_status_console(len(os.listdir(DATA_PATH)), current_file, start_time)
     articles = get_parsed_articles(path)
     articles = add_tokens_to_articles(articles)
     insert_articles_tokens(articles)
@@ -17,11 +17,11 @@ def process_insert_articles(path, current_file, start_time):
 def main():
     console_confirmation()
     start_time = datetime.now()
-    files = os.listdir(TEST_DATA_PATH)
+    files = os.listdir(DATA_PATH)
     try:
-        for (root, dirs, files) in os.walk(TEST_DATA_PATH):
+        for (root, dirs, files) in os.walk(DATA_PATH):
             for index, file in enumerate(files):
-                process_insert_articles(os.path.join(TEST_DATA_PATH, file), index, start_time)
+                process_insert_articles(os.path.join(DATA_PATH, file), index, start_time)
     except HttpException as e:
         print_error('HttpException: ' + str(e))
 main()
