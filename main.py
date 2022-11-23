cda4d71693bb47a4f5ff66efc51332c7f563928f
knielsen-afk <knie20@student.aau.dk>
2022-11-23 12:19:11 +0100

    progress on clustering

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tokens.gen_vec_words import store_word_vecs_template
 from tokens.words_vecs import get_word_vecs
 from articles.articles_handler import insert_arts_db
 from console import print_menu
+from vec_processing.kmeans_clustering import do_clustering
 
 DATA_PATH = 'jsonTestData/'
 STORAGE_PATH = 'storage_data/'
@@ -14,7 +15,8 @@ def main():
         case '2':
             store_word_vecs_template(API_URL, STORAGE_PATH)
         case '3':
-            get_word_vecs(API_URL, STORAGE_PATH)
+            word_vecs = get_word_vecs(API_URL, STORAGE_PATH)
+            do_clustering(word_vecs)
         case _:
             print('Input was not correct')
 main()
--- vec_processing/kmeans_clustering.py
+++ vec_processing/kmeans_clustering.py
@@ -2,34 +2,40 @@ from sklearn.cluster import KMeans
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-from data_handler.fetch import fetch_tokens
 
-API_URL = 'http://localhost:5501/document-data-api/'
+def split_2d_list(input):
+    vecs = []
+    ids = []
+    for word_vec in input:
+        vecs.append(word_vec['vec'])
+        ids.append(word_vec['id'])
+    return vecs, ids
+    
+def do_clustering(input):
+    vecs, ids = split_2d_list(input)
+    array = np.array(vecs)
+    print(array)
+    
+    # x = np.random.rand(400000, 2000)  # (antal af lister, antal af dimensioner)
+    start_time = time.time()
+    # Find optimal amount of cluster with elbow method
+    # intertia = []
+    # for i in range(1, 30):
+    #     print("i: ", i)
+    #     kmeans = KMeans(i)
+    #     kmeans.fit(array)
+    #     intertia.append(kmeans.inertia_)
+    #     print("--- %s seconds ---" % (time.time() - start_time))
+    # print("--- Total time: %s seconds ---" % (time.time() - start_time))
+    # number_clusters = range(1, 30)
+    # plt.plot(number_clusters, intertia)
+    # plt.title('The Elbow')
+    # plt.xlabel('Number of clusters')
+    # plt.ylabel('Intertia')
+    # plt.show()
 
-x = fetch_tokens(API_URL, 30, 0)
-# x = np.random.rand(400000, 2000)  # (antal af lister, antal af dimensioner)
-print(x)
-
-start_time = time.time()
-# Find optimal amount of cluster with elbow method
-# intertia = []
-# for i in range(1, 10):
-#     print("i: ", i)
-#     kmeans = KMeans(i)
-#     kmeans.fit(x)
-#     temp_iter = kmeans.inertia_
-#     intertia.append(temp_iter)
-#     print("--- %s seconds ---" % (time.time() - start_time))
-# print("--- Total time: %s seconds ---" % (time.time() - start_time))
-# number_clusters = range(1, 10)
-# plt.plot(number_clusters, intertia)
-# plt.title('The Elbow')
-# plt.xlabel('Number of clusters')
-# plt.ylabel('Intertia')
-# plt.show()
-
-# Performing the K-Means clustering algorithm
-# kmeans = KMeans(n_clusters=10).fit(x)
-# print("--- Total time: %s seconds ---" % (time.time() - start_time))
-# labels = kmeans.labels_
-# print(labels)
+    # Performing the K-Means clustering algorithm
+    kmeans = KMeans(n_clusters=14).fit(array)
+    print("--- Total time: %s seconds ---" % (time.time() - start_time))
+    labels = kmeans.labels_
+    print(labels)
