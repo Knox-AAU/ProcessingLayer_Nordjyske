190f6fcf0a4c4e4daa80fb5dfbdfa4e95bab4ce5
Patrickggwpez <73360625+Patrickggwpez@users.noreply.github.com>
2022-11-30 14:49:05 +0100

    ksmeans test og parser tests
    test_split_2d_list og test_get_token_count

---
+++ tests/kmeans_clustering_test.py
@@ -0,0 +1,17 @@
+import unittest
+from vec_processing.kmeans_clustering import split_2d_list
+
+
+class TestKMeansClustering (unittest.TestCase):
+    def test_split_2d_list(self):
+        word_vecs = [{'id': 1, 'vec': [1, 2, 3]},
+                     {'id': 2, 'vec': [1, 2, 3]},
+                     {'id': 33, 'vec': [11111]},
+                     {'id': (), 'vec': []},
+                     {'id': (), 'vec': [9, 8, 7]},
+                     {'id': (4), 'vec': []}]
+
+        expected_results = (
+            [[1, 2, 3], [1, 2, 3], [11111], [], [9, 8, 7], []],
+            [1, 2, 33, (), (), 4])
+        self.assertEqual(split_2d_list(word_vecs), expected_results)
--- tests/parser_test.py
+++ tests/parser_test.py
@@ -3,6 +3,7 @@ import json
 from articles import article_class
 from articles.json_parser import get_paragraph
 from articles.json_parser import remove_duplicates
+from articles.json_parser import get_token_count
 
 
 class TestParserGetParagraph(unittest.TestCase):
@@ -31,7 +32,8 @@ class TestParserGetParagraph(unittest.TestCase):
                         {"kind":"paragraph", "value":"2.1"}]
                     """
         data = json.loads(json_data)
-        self.assertEqual(get_paragraph(data), (['sub1 ', 'sub2 '], ['1.1 1.2 ', '2.1 ']))
+        self.assertEqual(get_paragraph(
+            data), (['sub1 ', 'sub2 '], ['1.1 1.2 ', '2.1 ']))
 
     def test_get_paragraph_append_all_paragraphs_subheaders_sub2_empty(self):
         json_data = """[{"kind":"subheader", "value":"sub1"},
@@ -40,7 +42,8 @@ class TestParserGetParagraph(unittest.TestCase):
                         {"kind":"subheader", "value":"sub2"}]
                     """
         data = json.loads(json_data)
-        self.assertEqual(get_paragraph(data), (['sub1 ', 'sub2 '], ['1.1  1.2 ', '']))
+        self.assertEqual(get_paragraph(
+            data), (['sub1 ', 'sub2 '], ['1.1  1.2 ', '']))
 
     def test_get_paragraph_add_all_subheaders(self):
         json_data = """[{"kind":"subheader", "value":"sub1"},
@@ -71,8 +74,8 @@ class TestParserGetParagraph(unittest.TestCase):
         arts.append(art1)
         arts.append(art2)
         result = remove_duplicates(arts)
-        self.assertTrue(len(result)==2)
-        
+        self.assertTrue(len(result) == 2)
+
     def test_is_not_duplicate_2(self):
         arts = []
         art1 = article_class.ArticleClass(
@@ -87,6 +90,29 @@ class TestParserGetParagraph(unittest.TestCase):
         arts.append(art2)
         result = remove_duplicates(arts)
         self.assertEqual(result, [art2])
+
+    def test_get_token_count_no_count(self):
+        art = article_class.ArticleClass(
+            headline='', publication='publication', author_name='name')
+        art.body_text = ''
+        art.sub_head = ''
+        actual_token_count = 0
+        self.assertEqual(get_token_count(art), actual_token_count)
+
+    def test_parameterized_get_token_count(self):
+        art = article_class.ArticleClass(
+            headline='headline', publication='publication', author_name='name')
+        param_list = [('ming', 'mang', 3),
+                      ('ming ming', 'mang', 4),
+                      ('mingmang', '', 2),
+                      ('', 'mingmang', 2),
+                      ('', '', 1),
+                      ('i', 'i', 1)]
+        for body_text, sub_head, expected_count in param_list:
+            with self.subTest():
+                art.body_text = body_text
+                art.sub_head = sub_head
+                self.assertEqual(get_token_count(art), expected_count)
 
 
 if __name__ == '__main__':
