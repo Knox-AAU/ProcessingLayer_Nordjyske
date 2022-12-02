482b29071960e486d4c7af37901def9df8b0e71d
JakobJFL <jlykke20@student.aau.dk>
2022-12-02 15:44:53 +0100

    fixed test

--- tests/topics_nearest_doc_test.py
+++ tests/topics_nearest_doc_test.py
@@ -12,11 +12,11 @@ class TestTopicsNearestDoc(unittest.TestCase):
         ]
         db_ids = [1, 5, 77, 2]
         self.assertEqual(update_topics(topics, db_ids), [
-            {'id': 1, 'topic': 1},
-            {'id': 1, 'topic': 5},
-            {'id': 1, 'topic': 77},
-            {'id': 1, 'topic': 5},
-            {'id': 1, 'topic': 2}
+            {'id': 1, 'category': 1},
+            {'id': 1, 'category': 5},
+            {'id': 1, 'category': 77},
+            {'id': 1, 'category': 5},
+            {'id': 1, 'category': 2}
         ])
 
     def test_update_topics_no_match(self):
